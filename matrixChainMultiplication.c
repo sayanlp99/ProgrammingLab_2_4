ffee58787991cc4e33450ffc6c3ae20d54648048
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-02-13 08:24:05 -0500

    Update matrixChainMultiplication.c

--- matrixChainMultiplication.c
+++ matrixChainMultiplication.c
@@ -11,9 +11,9 @@ int main(){
     for(i=1;i<=n;i++){
         m[i][i]=0;
     }
-    for(l=1;l<n;l++){
-        for(i=1;i<n-l;i++){
-            j=i+1;
+    for(l=2;l<n;l++){
+        for(i=1;i<n-l+1;i++){
+            j=i+l-1;
             m[i][j]=9999;
             for(k=i;k<=j-1;k++){
                 q=m[i][k]+m[k+1][j]+(p[i-1]*p[k]*p[j]);
@@ -23,6 +23,6 @@ int main(){
             }
         }
     }
-    printf("The number of steps in the Multiplication: %d", m[1][n]);
+    printf("The number of steps in the Multiplication: %d", m[1][n-1]);
     return 0;
 }
