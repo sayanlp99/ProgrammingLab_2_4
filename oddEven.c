a63ab2e8c8f7b62744e8a415c5f63771343a864b
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-02-18 16:01:42 +0530

    Create oddEven.c

---
+++ oddEven.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    int a[10000],b[10000],i,n,j,k,temp,c=0;
+    printf("Enter size of the  array : ");
+    scanf("%d", &n);
+    printf("Enter elements in array :\n");
+    for(i=0; i<n; i++){
+        printf("[%d]: ",i+1);
+        scanf("%d",&a[i]);
+        if(a[i]%2==1)
+            c++;
+    }
+    for(i=0; i<n-1; i++){      
+        for(j=0; j<n-i-1; j++){
+            if(a[j]>a[j+1]){
+            temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+            }
+        }
+    }
+    k=0;
+    j=n-c;
+    for(i=0; i<n; i++){
+        if(a[i]%2==0){
+            if(k<n-c)
+                b[k++]=a[i];
+        }  
+        else{
+            if(j<n)
+                b[j++]=a[i];
+        }
+    }
+    printf("Array after sorting even and odd elements separately: ");
+    for(i=0; i<n; i++){
+        a[i]=b[i];
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+ }
