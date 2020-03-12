117e24a51b18c3ebd5ab39397f96ecd3ff101fbd
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-03-12 16:09:34 +0530

    Add files via upload

---
+++ VHDL/fullAdderStructural/andGate.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:27:06 03/12/2020 
+-- Design Name: 
+-- Module Name:    andGate - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity andGate is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           C : out  STD_LOGIC);
+end andGate;
+
+architecture Behavioral of andGate is
+
+begin
+
+C <= A and B;
+
+end Behavioral;
+
