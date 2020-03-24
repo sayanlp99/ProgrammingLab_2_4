dcc66c40f586c18a71c6334d555137b6765d83a4
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-03-24 13:12:39 +0530

    Update matrixChainMultiplication.c

--- C/matrixChainMultiplication.c
+++ C/matrixChainMultiplication.c
@@ -1,29 +1,33 @@
 #include<stdio.h>
 #include<limits.h>
-int main(){    
-    int n,m[10][10],p[10],l,j,k,q,i=0;
+int main(){
+    int n,i;
+    int j, k, L, q;
     printf("Enter number of matrices: ");
     scanf("%d",&n);
-    printf("Enter dimensions of the matrices:\n");
-    for(i=0;i<=n;i++){
-        printf("[d%d]:",i+1);
-        scanf("%d",&p[i]);
+    n++;
+    int arr[n];
+    printf("Enter dimensions:\n");
+    for(i=0;i<n;i++){
+        printf("[d%d]: ",i+1);
+        scanf("%d",&arr[i]);
     }
-    for(i=1;i<n;i++){
-        m[i][i]=0;
-    }
-    for(l=2;l<n;l++){
-        for(i=1;i<n-l+1;i++){
-            j=i+l-1;
-            m[i][j]=INT_MAX;
-            for(k=i;k<=j-1;k++){
-                q=m[i][k]+m[k+1][j]+(p[i-1]*p[k]*p[j]);
-                if(q<m[i][j]){
-                    m[i][j]=q;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int m[size][size];
+    for (i=1; i<size; i++)
+        m[i][i] = 0;
+    for (L=2; L<size; L++){
+        for (i=1; i<size-L+1; i++){
+            j = i+L-1;
+            m[i][j] = INT_MAX;
+            for (k=i; k<=j-1; k++){
+                q = m[i][k] + m[k+1][j] + arr[i-1]*arr[k]*arr[j];
+                if (q < m[i][j]){
+                    m[i][j] = q;
                 }
             }
         }
     }
-    printf("The number of steps in the Multiplication: %d\n", m[1][n-1]);
+    printf("Minimum number of multiplications is %d \n", m[1][n-1]);
     return 0;
 }
