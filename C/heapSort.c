5d6bbc5482bce68bcfc70e35b0b2d5fbfc3acca6
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-06-17 19:32:32 +0530

    Add files via upload

---
+++ C/heapSort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void maxHeapify(int* a, int size, int i) {
+	int t, largest = i, left  = (2*i)+1, right  = (2*i)+2;
+	if (left<size && *(a+left) > *(a+largest)){
+		largest = left;
+	}
+	if (right  < size && *(a+right) > *(a+largest)){
+		largest = right;
+	}
+	if (largest != i) {
+	    t = *(a+i); *(a+i) = *(a+largest);
+	    *(a+largest) = t;
+	    maxHeapify(a, size, largest);
+	}
+}
+void heapSort(int* a, int n) {
+ 	int i,t;
+	for (i = n/2 - 1; i >= 0; i--){
+    		maxHeapify(a, n, i);
+	}
+	for (i = n - 1; i >= 0; i--) {
+		t = *a; *a = *(a+i);
+		*(a+i) = t;
+		maxHeapify(a, i, 0);
+	}
+}
+void main(){
+	int size,i,array[20];
+	printf("Enter size of the array: ");
+	scanf("%d",&size);
+	printf("Enter array elements:-\n");
+	for(i=0;i<size;i++)
+		scanf("%d",&array[i]);
+	heapSort(array,size);
+	printf("Array after sorting:-\n");
+	for(i=0;i<size;i++){
+		printf("%d ",array[i]);
+	}
+	printf("\n");
+}
