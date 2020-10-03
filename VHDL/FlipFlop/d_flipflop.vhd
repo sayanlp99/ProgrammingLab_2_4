4b06bfc235411181ff7a72427d073efb29c5bcd9
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-10-04 01:45:00 +0530

    Update d_flipflop.vhd

--- VHDL/FlipFlop/d_flipflop.vhd
+++ VHDL/FlipFlop/d_flipflop.vhd
@@ -1,31 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    11:45:24 04/26/2020 
--- Design Name: 
--- Module Name:    sr_flipflop - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity d_flipflop is
     Port ( d : in  STD_LOGIC;
