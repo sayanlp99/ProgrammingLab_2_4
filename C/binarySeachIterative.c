a817b41c28df5d362e7f2b1403f15b10e4624d12
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2020-10-10 01:48:43 +0530

    Update binarySeachIterative.c

--- C/binarySeachIterative.c
+++ C/binarySeachIterative.c
@@ -1,34 +1,29 @@
-#include<stdio.h>
-int n, l, u, i, a[100], item, pos, m;
-int binarySearch(int l, int u, int item, int a[100]);
-int main(){
+#include <stdio.h> 
+int binarySearch(int arr[], int l, int r, int x) { 
+    while (l <= r) { 
+        int m = l + (r - l) / 2; 
+        if (arr[m] == x) 
+            return m; 
+        if (arr[m] < x) 
+            l = m + 1;
+        else
+            r = m - 1; 
+    } 
+    return -1; 
+} 
+  
+int main(void) { 
+    int arr[20], n, x;
     printf("Enter number of elements: ");
     scanf("%d",&n);
     printf("Enter elements:\n");
-    for(i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){
         printf("[%d]: ",i+1);
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
     printf("Enter Element to be searched: ");
-    scanf("%d", &item);
-    l = 0;
-    u = n-1;
-    pos = binarySearch(l, u, item, a);
-    if (pos == -1)
-        printf("Element not found\n");
-    else
-        printf("Element %d, found at Position %d",a[pos] , pos+1);
-    return 0;
-}
-int binarySearch(int l, int u, int item, int a[100]){
-    while(l <= u){
-        m = (l+u)/2;
-        if(a[m] == item)
-            return m;
-        else if(item < a[m])
-            u = m-1;
-        else
-            l = m+1;
-    }
-    return -1;
-}
+    scanf("%d", &x);
+    int result = binarySearch(arr, 0, n - 1, x); 
+    (result == -1) ? printf("Element is not present in array") : printf("Element is present at index %d", result); 
+    return 0; 
+} 
